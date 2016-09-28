a35b44ebd817ddd012e0b6a413e18cab10895624
grafaa <grafaa@hotmail.fr>
2016-09-28 18:44:00 +0200

    Fix User Service

--- ClayApplication.Service/UserService.cs
+++ ClayApplication.Service/UserService.cs
@@ -1,45 +1,121 @@
 ﻿using ClayApplication.DataAccess;
 using ClayApplication.Domain.Abstract;
 using ClayApplication.Service.ViewModel;
+using ClayApplication.Service.Extension;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClayApplication.Service
 {
     public class UserService: IUserService
     {
-        private readonly IRepo<User> repo;
-        public UserService(IRepo<User> repo)
+        private readonly IRepo<User> userRepo;
+        private readonly IRepo<DoorAccess> doorAccessRepo;
+        public UserService(IRepo<User> userRepo, IRepo<DoorAccess> doorAccessRepo)
         {
-            this.repo = repo;  
-        }
-
-        private UserViewModel ConvertToViewModel(User user)
-        {
-            var userViewModel = new UserViewModel()
-            {
-                Id = user.id,
-                LastName = user.LastName,
-                FirstName = user.FirstName,
-                Login = user.login
-            };
-
-            return userViewModel;
+            this.userRepo = userRepo;
+            this.doorAccessRepo = doorAccessRepo;
         }
 
         public UserViewModel GetUser(string login, string pwd)
         {
-            var user = repo.Where(x => x.login.Equals(login) && x.pwd.Equals(pwd));
-            return ConvertToViewModel(user as User);
+            var user = userRepo.Where(x => x.login.Equals(login) && x.pwd.Equals(pwd));
+            if(user != null)
+            {
+                return user.First().GetViewModel();
+            }
+            
+            return null;
         }
 
         public UserViewModel GetUser(int id)
         {
-            var user = repo.Get(id);
-            return ConvertToViewModel(user);
+            var user = userRepo.Get(id);
+            return user.GetViewModel();
+        }
+
+        public bool DoesUserExist(string login, string pwd)
+        {
+            bool found = false;
+            try
+            {
+                var userQueryable = userRepo.Where(x => x.login == login && x.pwd == pwd);
+                found = userQueryable != null;
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            return found;
+        }
+
+        public ICollection<UserViewModel> GetAll()
+        {
+            var users = new List<UserViewModel>();
+            try
+            {
+                var usersObjects = userRepo.GetAll();
+                if (usersObjects != null)
+                {
+                    foreach (var userObject in usersObjects)
+                    {
+                        users.Add(userObject.GetViewModel());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            return users;
+        }
+
+        public IEnumerable<DoorViewModel> GetAllDoorsByUserId(int userId)
+        {
+            var doors = new List<DoorViewModel>();
+            try
+            {
+                var user = userRepo.Get(userId);
+                if (user != null)
+                {
+                    foreach (var door in user.Door)
+                    {
+                        doors.Add(door.GetViewModel());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+            
+            return doors;
+        }
+
+        public IEnumerable<DoorViewModel> DoorsUserHasAccess(int id)
+        {
+            var doors = new List<DoorViewModel>();
+            try
+            {
+                var doorAccessObjects = doorAccessRepo.Where(x => x.userid == id);
+                var doorAccessObjectList = doorAccessObjects.ToList();
+                if (doorAccessObjects != null)
+                {
+                    foreach (var doorAccessObject in doorAccessObjectList)
+                    {
+                        doors.Add(doorAccessObject.Door.GetViewModel());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            return doors;
         }
     }
 }
